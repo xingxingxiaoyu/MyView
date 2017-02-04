74de06c65edc8aea7bbc0abfea3be348de94608f
xingxingxiaoyu <1062654151@qq.com>
2017-02-04 09:54:19 +0800

    first

---
+++ app/src/main/java/com/xuyu/myview/SideSlipe/FourthActivity.java
@@ -0,0 +1,18 @@
+package com.xuyu.myview.SideSlipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xuyu.myview.R;
+
+public class FourthActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+
+    }
+}
---
+++ app/src/main/java/com/xuyu/myview/SideSlipe/SideSlipeView.java
@@ -0,0 +1,17 @@
+package com.xuyu.myview.SideSlipe;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by Xuyu on 2017/2/4.
+ */
+public class SideSlipeView extends ScrollView
+{
+
+    public SideSlipeView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+}
