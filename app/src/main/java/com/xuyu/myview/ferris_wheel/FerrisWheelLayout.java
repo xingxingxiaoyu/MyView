041502d55b7375acc958942f75fea3693f97e2a2
xingxingxiaoyu <xujiafeng_31415926@163.com>
2017-03-27 12:45:27 +0800

    add FerrisWheelLayout

--- app/src/main/java/com/xuyu/myview/ferris_wheel/FerrisWheelLayout.java
+++ app/src/main/java/com/xuyu/myview/ferris_wheel/FerrisWheelLayout.java
@@ -1,11 +1,15 @@
 package com.xuyu.myview.ferris_wheel;
 
 import android.content.Context;
-import android.icu.math.MathContext;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by: xuyu
@@ -16,8 +20,10 @@ import android.widget.LinearLayout;
  */
 public class FerrisWheelLayout extends ViewGroup
 {
-
+    private double angle = 0F;
     private int DEFAULT_SIZE = 200;
+    private String TAG = "FerrisWheelLayout";
+    private Paint mPaint;
 
     public FerrisWheelLayout(Context context, AttributeSet attrs, int defStyleAttr)
     {
@@ -37,7 +43,9 @@ public class FerrisWheelLayout extends ViewGroup
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr)
     {
-
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.BLACK);
+        setWillNotDraw(false);
     }
 
     @Override
@@ -46,6 +54,14 @@ public class FerrisWheelLayout extends ViewGroup
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int width = measure(widthMeasureSpec);
         int height = measure(widthMeasureSpec);
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++)
+        {
+            View childAt = getChildAt(i);
+            LayoutParams layoutParams = childAt.getLayoutParams();
+            layoutParams.width = Dp2Px(FerrisWheelLayout.this.getContext(), 40);
+            layoutParams.height = Dp2Px(FerrisWheelLayout.this.getContext(), 40);
+        }
         int length = Math.min(width, height);
         setMeasuredDimension(length, length);
     }
@@ -67,39 +83,79 @@ public class FerrisWheelLayout extends ViewGroup
         }
     }
 
+    private int centerX;
+    private int centerY;
+    private int radius;
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b)
     {
-        int centerX = l + (r - l) / 2;
-        int centerY = t + (b - t) / 2;
-        int radius = Math.min((r - l) / 2, (b - t) / 2) - 30;
+        Log.e(TAG, "l" + l + " t" + t + " r" + r + " b" + b);
+        centerX = (r - l) / 2;
+        centerY = (b - t) / 2;
+        radius = Math.min((r - l) / 2, (b - t) / 2) - 30;
+        Log.e(TAG, "centerX:" + centerX + "centerY:" + centerY + "radius:" + radius);
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++)
         {
+            final int index = i;
             View child = getChildAt(i);
-            int childWidth = child.getMeasuredWidth();
-            int childHeight = child.getMeasuredHeight();
-            child.layout(centerX + (int) (radius * Math.cos(Math.PI * 2 / childCount * i)) - childWidth / 2,
-                    centerY + (int) (radius * Math.cos(Math.PI * 2 / childCount * i)) - childHeight / 2,
-                    centerX + (int) (radius * Math.cos(Math.PI * 2 / childCount * i)) + childWidth / 2,
-                    centerY + (int) (radius * Math.cos(Math.PI * 2 / childCount * i)) + childWidth / 2);
+            LayoutParams layoutParams = child.getLayoutParams();
+            int childWidth = layoutParams.width;
+            int childHeight = layoutParams.height;
+
+            child.layout(centerX + (int) (radius * Math.cos((Math.PI * 2) / childCount * i + angle)) - childWidth / 2,
+                    centerY + (int) (radius * Math.sin((Math.PI * 2) / childCount * i + angle)) - childHeight / 2,
+                    centerX + (int) (radius * Math.cos((Math.PI * 2) / childCount * i + angle)) + childWidth / 2,
+                    centerY + (int) (radius * Math.sin((Math.PI * 2) / childCount * i + angle)) + childWidth / 2);
+            child.setOnClickListener(new OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    Toast.makeText(FerrisWheelLayout.this.getContext(), "这是第" + index + "张图片", Toast.LENGTH_SHORT).show();
+                }
+            });
+            Log.e(TAG, i + " left:" + (centerX + (int) (radius * Math.cos((Math.PI * 2) / childCount * i + angle)) - childWidth / 2) +
+                    " top:" + (centerY + (int) (radius * Math.sin((Math.PI * 2) / childCount * i + angle)) - childHeight / 2) +
+                    " right:" + (centerX + (int) (radius * Math.cos((Math.PI * 2) / childCount * i + angle)) + childWidth / 2) +
+                    " bottom:" + (centerY + (int) (radius * Math.sin((Math.PI * 2) / childCount * i + angle)) + childWidth / 2));
         }
     }
-    class LayoutParams extends ViewGroup.LayoutParams
+
+    @Override
+    protected void onDraw(Canvas canvas)
     {
-        public LayoutParams(Context c, AttributeSet attrs)
+        super.onDraw(canvas);
+        canvas.translate(centerX, centerY);
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++)
         {
-            super(c, attrs);
+            canvas.drawLine(0, 0, (int) (radius * Math.cos((Math.PI * 2) / childCount * i + angle))
+                    , (int) (radius * Math.sin((Math.PI * 2) / childCount * i + angle)), mPaint);
         }
+        canvas.translate(-centerX, -centerY);
+    }
 
-        public LayoutParams(int width, int height)
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch (event.getAction())
         {
-            super(width, height);
+//            case MotionEvent
         }
+        return super.onTouchEvent(event);
+    }
 
-        public LayoutParams(ViewGroup.LayoutParams source)
-        {
-            super(source);
-        }
+    public int Dp2Px(Context context, float dp)
+    {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+    public int Px2Dp(Context context, float px)
+    {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5f);
     }
 }
