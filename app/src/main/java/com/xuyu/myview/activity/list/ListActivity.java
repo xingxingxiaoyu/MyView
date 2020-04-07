7ebd62180fe162efe0ffe2e8112215d0ae663989
徐郁 <xuyu@konka.com>
2020-04-07 14:14:34 +0800

    添加装逼小飞机

--- app/src/main/java/com/xuyu/myview/activity/list/ListActivity.java
+++ app/src/main/java/com/xuyu/myview/activity/list/ListActivity.java
@@ -1,15 +1,67 @@
 package com.xuyu.myview.activity.list;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 
 import com.xuyu.myview.R;
 
 public class ListActivity extends AppCompatActivity {
+    int i = 0;
+    private PathView currentPathView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        int screenWidthDp = getResources().getConfiguration().screenWidthDp;
+        final float scale = getResources().getDisplayMetrics().density;
+        recyclerView.setAdapter(new PathAdapter(this, (int) (screenWidthDp * scale + 0.5f)));
+
+        recyclerView.setClipChildren(false);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                Log.e("====", "" + dy);
+                int childCount = recyclerView.getChildCount();
+                for (int i = 0; i < childCount; i++) {
+                    PathView child = (PathView) recyclerView.getChildAt(i);
+                    float line = (getResources().getConfiguration().screenHeightDp * scale + 0.5f) / 3;
+                    Log.e("====", "i " + i + " child.getTop() " + child.getTop());
+                    if (child.getTop() > line) {
+                        if (child != currentPathView) {
+                            if (currentPathView != null) {
+                                currentPathView.setProgress(-1);
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                    currentPathView.setZ(0);
+                                }
+                            }
+                            currentPathView = child;
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                currentPathView.setZ(1);
+                            }
+                        }
+                        int progress = (int) ((child.getTop() - line) / child.getHeight() * 100);
+                        Log.e("====", "progress " + progress);
+                        child.setProgress(100 - progress);
+                        return;
+                    }
+                }
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/xuyu/myview/activity/list/PathView.java
@@ -0,0 +1,99 @@
+package com.xuyu.myview.activity.list;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 描述信息：
+ *
+ * @author xuyu
+ * @date 2020/4/7
+ */
+public class PathView extends View {
+
+    private Path mPath;
+    private Paint mPaint;
+    private int mProgress = -1;
+    private Bitmap mBitmap;
+    private Rect mRect;
+    private float[] pos = new float[2];
+    private float[] tan = new float[2];
+
+    public PathView(Context context) {
+        this(context, null);
+    }
+
+    public PathView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(0xff123456);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(40);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+    }
+
+    public void setPath(Path path) {
+        mPath = path;
+        postInvalidate();
+    }
+
+    public void setProgress(int progress) {
+        mProgress = progress;
+        if (mPath != null) {
+            PathMeasure pathMeasure = new PathMeasure(mPath, false);
+            float length = pathMeasure.getLength();
+            pathMeasure.getPosTan(progress / 100f * length, pos, tan);
+            Log.e("===", "pos " + pos[0] + " " + pos[1] + " " + "tan " + tan[0] + " " + tan[1]);
+        }
+        postInvalidate();
+    }
+
+    public void setImage(int imageRes) {
+        mBitmap = BitmapFactory.decodeResource(getResources(), imageRes);
+        int width = mBitmap.getWidth();
+        int height = mBitmap.getHeight();
+        mRect = new Rect(-width / 2, -height / 2, width / 2, height / 2);
+        postInvalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawPath(mPath, mPaint);
+        if (mProgress >= 0) {
+            canvas.save();
+            canvas.translate(pos[0], pos[1]);
+            double atan = Math.atan(tan[1] / tan[0]);
+            float degrees = (float) (atan * 180 / Math.PI);
+            Log.e("===", "degrees " + degrees);
+            degrees += 90;
+            if (tan[0] < 0) {
+                degrees += 180;
+            }
+            canvas.rotate(degrees);
+            canvas.drawBitmap(mBitmap, null, mRect, mPaint);
+            canvas.restore();
+        }
+    }
+
+
+}
